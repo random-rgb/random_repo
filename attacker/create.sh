f2dce222f4075cd46481bc7b225bb06c1805e860
avram-avram <avram-avram@tutanota.com>
2020-12-14 18:10:04 +0200

    ccc

--- attacker/create.sh
+++ attacker/create.sh
@@ -2,4 +2,4 @@
 container_name=attacker_$1
 docker run --rm -idt --name $container_name ubuntu
 docker cp ./installations.sh $container_name:/home/installations.sh
-docker exec -it $container_name sh installations.sh
+docker exec -it $container_name sh /home/installations.sh
