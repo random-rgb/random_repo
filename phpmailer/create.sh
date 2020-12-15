80ebdfec5c58d278a79c48e8de9e2672d7b6ad84
avram-avram <avram-avram@tutanota.com>
2020-12-15 18:53:26 +0200

    asdacvas

--- phpmailer/create.sh
+++ phpmailer/create.sh
@@ -1,6 +1,5 @@
 # PHPmailer vulnerability
 
 # https://github.com/opsxcq/exploit-CVE-2016-10033.git
-cd exploit-CVE-2016-10033
 docker build --tag first_webserver_base_image .
 docker run --rm -it --name first_webserver_$1 -p 8080:80 first_webserver_base_image
