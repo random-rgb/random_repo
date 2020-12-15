0ba41244edeb7533c3ce465bda59b73a820c1ad7
avram-avram <avram-avram@tutanota.com>
2020-12-15 19:07:11 +0200

    dfhhg

--- phpmailer/create.sh
+++ phpmailer/create.sh
@@ -1,5 +1,6 @@
 # PHPmailer vulnerability
 
 # https://github.com/opsxcq/exploit-CVE-2016-10033.git
+chmod 777 main.sh
 docker build --tag first_webserver_base_image .
 docker run --rm -it --name first_webserver_$1 -p 8080:80 first_webserver_base_image
