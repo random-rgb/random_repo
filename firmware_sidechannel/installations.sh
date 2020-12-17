5ebaa234f16dcfdb4ab9975224139a0f5f68d8a5
avram-avram <avram-avram@tutanota.com>
2020-12-17 14:24:20 +0200

    d

--- firmware_sidechannel/installations.sh
+++ firmware_sidechannel/installations.sh
@@ -3,7 +3,7 @@ apt update
 apt install -y curl tar python2 gcc
 curl https://bootstrap.pypa.io/get-pip.py --output /home/get-pip.py
 python2 home/get-pip.py
-python2 -m pip install requests flask flask-cors pyftpdlib
+python2 -m pip install requests flask flask-cors Flask-Scss pyftpdlib 
 gcc /home/webserver/authentication.c -o /home/webserver/authentication_provider
 rm -rf /home/webserver/authentication.c
 
--- firmware_sidechannel/webserver/webserver.py
+++ firmware_sidechannel/webserver/webserver.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask.ext.scss import Scss
+from flask_cors import CORS
 import hashlib, json
 import os
 import tarfile
 
 app = Flask(__name__, static_folder="./templates")
+CORS(app)
+Scss(app, static_dir='./templates', asset_dir='./templates')
 
 @app.route('/')
 def home():
@@ -16,12 +20,13 @@ def home():
 def the_main_website():
 	pass
 	
-
+@app.route('/moses')
 def upload_firmware():
-	tf = tarfile.TarFile("zip-slip.tar")
-	tf.list()
-	tf.extractall()
-	pass
+	# tf = tarfile.TarFile("zip-slip.tar")
+	# tf.list()
+	# tf.extractall()
+	return render_template('firmware_upload/asd.html')
+	
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
